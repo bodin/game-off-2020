53a2bbcdf7f99f67e8667777c2c76383ab48f868
Derek Bodin <derek.bodin@mckesson.com>
2020-11-01 09:28:32 -0600

    feat: add favicon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'phaser';
-import scene from './scene/demo'
+import scene_demo from './scene/demo'
 
 window.onload = () => {
     new Phaser.Game({
         type: Phaser.AUTO,
         width: 800,
         height: 600,
-        scene: scene
+        scene: [scene_demo]
     });
 };

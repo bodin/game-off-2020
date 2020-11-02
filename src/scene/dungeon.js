9c38e5173f18ca9bd7c693f096be5b7c59c6538e
Derek Bodin <derek.bodin@mckesson.com>
2020-11-01 19:55:48 -0600

    fix: refactor to constants

--- src/scene/dungeon.js
+++ src/scene/dungeon.js
@@ -1,40 +1,52 @@
 import {Scene} from 'phaser'
 import TILE from '../model/tiles'
 
+const TOP = 0
+const LEFT = 1
+const RIGHT = 2
+const BOTTOM = 3
+
+const DOOR_MISSING = 0;
+const DOOR_EXISTS = 1;
+const DOOR_UNDEFINED = -1;
+
 export default class DemoScene extends Scene {
+   
     constructor(config) {
         super(config);
+        this.map = []
     }
 
-    preload () {
+    preload () {        
         this.load.image("tiles", "../assets/tileset/default.png");
     }
 
     create () {
-        const room_layout = this.makeRoom([1,1,-1,-1])
-        // When loading from an array, make sure to specify the tileWidth and tileHeight
-        const map = this.make.tilemap({ data: this.makeRoomTiles(23, 23, room_layout), tileWidth: 24, tileHeight: 24 });
+        const room_layout = this.makeRoom()
+
+        const map = this.make.tilemap({ data: this.makeRoomTiles(25, 25, room_layout), tileWidth: 24, tileHeight: 24 });
         const tiles = map.addTilesetImage("tiles");
         const layer = map.createStaticLayer(0, tiles, 0, 0);
     }
-    makeRoom(allowDoors=[-1,-1,-1,-1]){
+
+    makeRoom(allowDoors=[DOOR_UNDEFINED,DOOR_UNDEFINED,DOOR_UNDEFINED,DOOR_UNDEFINED]){
         let doors = [...allowDoors]
         
-        if(allowDoors[0] == -1) doors[0] = Math.random() > .9 ? 1 : 0
-        if(allowDoors[1] == -1) doors[1] = Math.random() > .9 ? 1 : 0
-        if(allowDoors[2] == -1) doors[2] = Math.random() > .9 ? 1 : 0
-        if(allowDoors[3] == -1) doors[3] = Math.random() > .9 ? 1 : 0
+        if(allowDoors[TOP] == DOOR_UNDEFINED)       doors[0] = Math.random() > .9 ? DOOR_MISSING : DOOR_EXISTS
+        if(allowDoors[LEFT] == DOOR_UNDEFINED)      doors[1] = Math.random() > .9 ? DOOR_MISSING : DOOR_EXISTS
+        if(allowDoors[RIGHT] == DOOR_UNDEFINED)     doors[2] = Math.random() > .9 ? DOOR_MISSING : DOOR_EXISTS
+        if(allowDoors[BOTTOM] == DOOR_UNDEFINED)    doors[3] = Math.random() > .9 ? DOOR_MISSING : DOOR_EXISTS
 
         return doors
     }
-    makeRoomTiles(width, height, layout = [0,0,0,0]){
+    makeRoomTiles(width, height, doors = [DOOR_EXISTS,DOOR_EXISTS,DOOR_EXISTS,DOOR_EXISTS]){
 
         const roomTiles = [
             [TILE.CORNER_TOP_LEFT,TILE.WALL_TOP,TILE.CORNER_TOP_RIGHT],
             [TILE.WALL_RIGHT,TILE.BLANK,TILE.WALL_RIGHT],
-            [TILE.CORNER_BOTTOM_LEFT,TILE.WALL_BOTTOM,TILE.CORNER_BOTTOM_RIGHT],
-            
+            [TILE.CORNER_BOTTOM_LEFT,TILE.WALL_BOTTOM,TILE.CORNER_BOTTOM_RIGHT],            
         ]
+
         const room = new Array(height)
         let tileRow = 0;
         for(let i = 0; i < height; i++){
@@ -52,28 +64,28 @@ export default class DemoScene extends Scene {
         }
 
         //top
-        if(layout[0] == 0){
+        if(doors[TOP] == DOOR_EXISTS){
             room[0][(width-1)/2-1]   = TILE.DOOR_HOR_LEFT
             room[0][(width-1)/2]     = TILE.DOOR_HOR_MIDDLE
             room[0][(width-1)/2+1]   = TILE.DOOR_HOR_RIGHT
         }
 
         //left
-        if(layout[1] == 0){
+        if(doors[LEFT] == DOOR_EXISTS){
             room[(height-1)/2-1][0]   = TILE.DOOR_VER_TOP
             room[(height-1)/2][0]     = TILE.DOOR_VER_MIDDLE
             room[(height-1)/2+1][0]   = TILE.DOOR_VER_BOTTOM
         }
 
         //right
-        if(layout[2] == 0){
+        if(doors[RIGHT] == DOOR_EXISTS){
             room[(height-1)/2-1][width-1]   = TILE.DOOR_VER_TOP
             room[(height-1)/2][width-1]     = TILE.DOOR_VER_MIDDLE
             room[(height-1)/2+1][width-1]   = TILE.DOOR_VER_BOTTOM
         }
 
         //bottom
-        if(layout[3] == 0){
+        if(doors[BOTTOM] == DOOR_EXISTS){
             room[height-1][(width-1)/2-1]   = TILE.DOOR_HOR_LEFT
             room[height-1][(width-1)/2]     = TILE.DOOR_HOR_MIDDLE
             room[height-1][(width-1)/2+1]   = TILE.DOOR_HOR_RIGHT

cc29582275bf254afb79843f783cf0d6a985cc56
Derek Bodin <derek.bodin@gmail.com>
2020-11-09 18:13:16 -0600

    feat: sprites

--- src/model/constants.js
+++ src/model/constants.js
@@ -10,8 +10,8 @@ export const ROOM_TILE_HEIGHT = 25
 export const ROOM_WIDTH = TILE_WIDTH * ROOM_TILE_WIDTH
 export const ROOM_HEIGHT = TILE_HEIGHT * ROOM_TILE_HEIGHT
 
-export const COLUMNS = 3
-export const ROWS = 3
+export const COLUMNS = 4
+export const ROWS = 4
 
 export const MAP_WIDTH = 180
 export const MAP_HEIGHT = 180
